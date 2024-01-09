e924131c54b800e0063c42a2f58078d1c4c32bbf
Abhineet kumar <abhineetkumar@Abhineets-MacBook-Air.local>
2024-01-09 11:26:39 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const heading = React.createElement("h1",{id:"heading"},"Hello world from React")
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+[React.createElement("h1",{},"I am H1 Tag "),React.createElement("h2",{},"I am H2 Tag ")]))
+// what if we want to creat multiple siblings inside the div
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+const deliveryPromise = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        const deliveryArrived = Math.random() < .5;
+        if (deliveryArrived){
+            resolve("Your package has arrived");
+        }
+        else{
+            reject("Your package has not yet arrived")
+        }
+    },2000)
+});
+deliveryPromise.then((successMessage)=>console.log(successMessage)).catch((rejectMessage)=>console.log(rejectMessage));
+root.render(parent);
+
